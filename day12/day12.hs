12794bb8cb8dd43eae32d3b55b50f1ec56d70861
Aqissiaq <aqissiaq@gmail.com>
2021-12-13 12:22:17 +0100

    removed dependency on Data.Text in day 12

--- day12/day12.hs
+++ day12/day12.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
 import qualified Data.Map as M
-import qualified Data.Text as T
+import Data.List.Split
 import Data.Char
 
 type Node = String
 type Graph = M.Map Node [Node]
-
 
 countPaths :: Graph -> Node -> Node -> [Node] -> Bool -> Int
 countPaths graph u t visited allowRevisits
@@ -15,13 +13,9 @@ countPaths graph u t visited allowRevisits
   | all isUpper u    = sum [countPaths graph c t visited allowRevisits | c <- graph M.! u]
   | otherwise        = sum [countPaths graph c t (u:visited) allowRevisits | c <- graph M.! u]
 
-
-sSplitOn :: String -> String -> [String]
-sSplitOn sep = map T.unpack . T.splitOn (T.pack sep) . T.pack
-
 makeGraph :: String -> Graph
 makeGraph str = M.fromListWith (++) $ concatMap makeEdge (lines str)
-  where makeEdge line = let [src, dest] = sSplitOn "-" line in
+  where makeEdge line = let [src, dest] = splitOn "-" line in
                           [(src, [dest]), (dest, [src])]
 
 -- just hiding some "default" args
