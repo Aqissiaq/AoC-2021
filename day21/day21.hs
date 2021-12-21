f91c8c6bd5a5de01706f5cc8427a94d02533817b
Aqissiaq <aqissiaq@gmail.com>
2021-12-21 17:36:36 +0100

    Part 2, day 21. Also broke part 1 and honestly I don't understand how it ever worked in the first place

--- day21/day21.hs
+++ day21/day21.hs
@@ -1,3 +1,7 @@
+import Control.Monad
+import Data.Array
+import Data.Monoid
+
 type Roll = (Int, Int, Int)
 data Player = Player {pos :: Int, score :: Int}
   deriving Show
@@ -20,10 +24,36 @@ oneTurn (d1, d2, d3) (GS p1 (Player p2Pos p2Score) False rolls) = let newPos = w
   in GS p1 (Player newPos newScore) True (rolls + 3)
 
 wrapAround :: Int -> Int
-wrapAround n | n > 9 = (n `mod` 10) + 1
+-- wrapAround n | n > 9 = (n `mod` 10) + 1
+-- this works for part 1, for some reason???
+-- somehow cancels out forgetting to wrap the dice values¿¿
+wrapAround n | n > 10 = wrapAround $ n - 10
              | otherwise = n
 
 play :: Roll -> GameState -> Int
 play (d1,d2,d3) gs | score (p1 gs) >= 1000 = score (p2 gs) * dieRolls gs
                    | score (p2 gs) >= 1000 = score (p1 gs) * dieRolls gs
-                   | otherwise = let newRoll = (d3+1, d3+2, d3+3) in play newRoll $ oneTurn newRoll gs
+                   | otherwise = let newRoll = (d1+3, d2+3, d3+3)
+                     in play newRoll $ oneTurn newRoll gs
+
+solve2 :: Int -> Int -> Sum Int
+solve2 p1Start p2Start = uncurry max $ dp ! ((0, p1Start), (0, p2Start), True)
+  where
+    limits = (((0,1), (0,1), False), ((30, 10), (30, 10), True))
+    dp = listArray limits [countWins p | p <- range limits]
+    countWins :: ((Int, Int), (Int, Int), Bool) -> (Sum Int, Sum Int)
+    countWins ((p1Score, p1Pos), (p2Score, p2Pos), p1Active)
+      | p1Score >= 21 = (1,0)
+      | p2Score >= 21 = (0,1)
+      | p1Active      = mconcat [dp ! ((p1Score + p, p), (p2Score, p2Pos), False)
+                                | p <- positions p1Pos]
+      | otherwise     = mconcat [dp ! ((p1Score , p1Pos), (p2Score + p, p), True)
+                                | p <- positions p2Pos]
+    positions current = [wrapAround (current + roll) | roll <- rolls]
+    rolls = map sum $ replicateM 3 [1,2,3]
+
+main :: IO ()
+main = do putStr $ "Part 1: "
+          print $ play (1,2,3) input
+          putStr $ "Part 2: "
+          print . getSum $ solve2 8 3
